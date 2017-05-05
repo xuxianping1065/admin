e440f23b653603cdd47008c496c7fc96b1d793a0
xuxianping1065 <xuxianping1065@sina.com>
2017-05-05 17:09:01 +0800

    update

--- src/main/java/com/framework/admin/web/controller/MenuInfoController.java
+++ src/main/java/com/framework/admin/web/controller/MenuInfoController.java
@@ -18,7 +18,6 @@ import com.framework.admin.web.req.MenuInfoQueryReq;
 import com.framework.admin.web.req.MenuInfoUpdateReq;
 import com.framework.common.crud.Result;
 import com.framework.common.exception.BusinessException;
-import com.framework.common.http.HttpTemplate;
 
 @RestController
 @RequestMapping("/menu")
@@ -26,51 +25,45 @@ public class MenuInfoController {
 
 	@Autowired
 	private MenuInfoService menuInfoService;
-	
-	@RequestMapping(value="/query", method=RequestMethod.POST )
-	public Result query(MenuInfoQueryReq req){
+
+	@RequestMapping(value = "/query", method = RequestMethod.POST)
+	public Result query(MenuInfoQueryReq req) {
 		Map<String, Object> paraMap = new HashMap<String, Object>();
 		List<MenuInfo> list = menuInfoService.query(paraMap);
-		
+
 		return Result.ok().putData(list);
 	}
-	
-	@RequestMapping(value="/get/{id}", method=RequestMethod.POST )
-	public Result get(@PathVariable("id") Integer id){
+
+	@RequestMapping(value = "/get/{id}", method = RequestMethod.POST)
+	public Result get(@PathVariable("id") Integer id) {
 		MenuInfo menuInfo = menuInfoService.get(id);
-		
+
 		return Result.ok().putData(menuInfo);
 	}
-	
-	@RequestMapping(value="/add", method=RequestMethod.POST )
-	public Result add(final MenuInfoAddReq req){
-		HttpTemplate<Result> template  = new HttpTemplate<Result>(Result.class) {
-			@Override
-			public void doSomething() throws BusinessException {
-				MenuInfo menuInfo = new MenuInfo();
-				BeanUtils.copyProperties(req, menuInfo);
-				
-				menuInfoService.add(menuInfo);
-			}
-		};
-		
-		return template.operate();
-	}
-	
-	@RequestMapping(value="/update", method=RequestMethod.POST )
-	public Result update(MenuInfoUpdateReq req){
+
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public Result add(MenuInfoAddReq req) {
 		MenuInfo menuInfo = new MenuInfo();
 		BeanUtils.copyProperties(req, menuInfo);
-		
+
+		menuInfoService.add(menuInfo);
+		return Result.ok();
+	}
+
+	@RequestMapping(value = "/update", method = RequestMethod.POST)
+	public Result update(MenuInfoUpdateReq req) {
+		MenuInfo menuInfo = new MenuInfo();
+		BeanUtils.copyProperties(req, menuInfo);
+
 		menuInfoService.update(menuInfo);
 		return Result.ok();
 	}
-	
-	@RequestMapping(value="/delete", method=RequestMethod.POST )
-	public Result delete(Integer[] ids){
-		
+
+	@RequestMapping(value = "/delete", method = RequestMethod.POST)
+	public Result delete(Integer[] ids) {
+
 		menuInfoService.delete(ids);
-		
+
 		return Result.ok();
 	}
 }
